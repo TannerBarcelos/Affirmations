deeef7eb71a657ffd1a31cc00c8eba947c325902
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2022-05-12 18:28:23 -0700

    Finish REST API Scaffolding. Ready for actual work

--- backend/controllers/affirmationController.js
+++ backend/controllers/affirmationController.js
@@ -1,7 +1,60 @@
+/**
+ * @description Get all affirmations
+ * @method GET
+ * @param {*} request - express request object
+ * @param {*} response - express response object
+ */
 function getAllAffirmations(request, response) {
   response.status(200).json({ message: 'All Affirmations' });
 }
 
+/**
+ * @description Get a single affirmation
+ * @method GET
+ * @param {*} request - express request object
+ * @param {*} response - express response object
+ */
+function getSingleAffirmation(request, response) {
+  const { id } = request.params;
+  response.status(200).json({ message: `Get single affirmation by ID: ${id}` });
+}
+
+/**
+ * @description Create a new affirmation
+ * @method POST
+ * @param {*} request - express request object
+ * @param {*} response - express response object
+ */
+function createAffirmation(request, response) {
+  response.status(200).json({ message: 'Create Affirmation' });
+}
+
+/**
+ * @description Update an affirmation
+ * @method PUT
+ * @param {*} request - express request object
+ * @param {*} response - express response object
+ */
+function updateAffirmation(request, response) {
+  const { id } = request.params;
+  response.status(200).json({ message: `Updated affirmation by ID: ${id}` });
+}
+
+/**
+ * @description Delete an affirmation
+ * @method DELETE
+ * @param {*} request - express request object
+ * @param {*} response - express response object
+ */
+function deleteAffirmation(request, response) {
+  const { id } = request.params;
+  response.status(200).json({ message: `Deleted affirmation by ID: ${id}` });
+}
+
 module.exports = {
   getAllAffirmations,
+  getSingleAffirmation,
+  createAffirmation,
+  updateAffirmation,
+  deleteAffirmation,
 };
--- backend/routes/affirmationRoutes.js
+++ backend/routes/affirmationRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllAffirmations } = require('../controllers/affirmationController');
+const {
+  getAllAffirmations,
+  getSingleAffirmation,
+  createAffirmation,
+  updateAffirmation,
+  deleteAffirmation,
+} = require('../controllers/affirmationController');
 
 router.get('/', getAllAffirmations);
+router.get('/:id', getSingleAffirmation);
+router.post('/create', createAffirmation);
+router.put('/edit/:id', updateAffirmation);
+router.delete('/delete/:id', deleteAffirmation);
 
 module.exports = router;
