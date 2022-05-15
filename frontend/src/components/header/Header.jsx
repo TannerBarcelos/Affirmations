b895e1f0b7f362009a5456910048088cd89bab3b
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2022-05-14 18:37:52 -0700

    Layout for menu + other fixes

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  const [isShowing, setIsShowing] = useState(false);
+  return (
+    <>
+      <header className='header'>
+        <div className='logo'>
+          <Link to='/' className='nav-item-link-logo'>
+            Affirmations
+          </Link>
+        </div>
+        <ul className={`nav-list${isShowing ? ' responsive' : ''}`}>
+          <li className='nav-item'>
+            <Link to='login' className='nav-item-link'>
+              <i className='fa-solid fa-circle-user'></i>
+              <span style={{ display: 'inline-block', paddingLeft: '1rem' }}>
+                Login
+              </span>
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='register' className='nav-item-link'>
+              <i className='fa-solid fa-user-plus'></i>
+              <span style={{ display: 'inline-block', paddingLeft: '1rem' }}>
+                Register
+              </span>
+            </Link>
+          </li>
+          <li
+            className='nav-item-menu-icon'
+            onClick={(e) => setIsShowing(!isShowing)}
+          >
+            {!isShowing ? (
+              <i className='fa-solid fa-bars'></i>
+            ) : (
+              <i class='fa-solid fa-x'></i>
+            )}
+          </li>
+        </ul>
+      </header>
+      {isShowing && (
+        <div className='responsive-links'>
+          <ul className={`nav-list${isShowing ? ' responsive' : ''}`}>
+            <li className='nav-item'>
+              <Link to='login' className='nav-item-link'>
+                <i className='fa-solid fa-circle-user'></i>
+                <span style={{ display: 'inline-block', paddingLeft: '1rem' }}>
+                  Login
+                </span>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='register' className='nav-item-link'>
+                <i className='fa-solid fa-user-plus'></i>
+                <span style={{ display: 'inline-block', paddingLeft: '1rem' }}>
+                  Register
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+export default Header;
