45f9cc6f9947f2416c31ded89206034c56a872bf
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2022-05-24 16:39:00 -0700

    Base done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import moodIcons from '../assets/icons/moodIcons';
+import affirmationImages from '../assets/images/affirmationImages';
+
 import {
   getAffirmations,
   reset,
@@ -37,6 +40,56 @@ const Dashboard = () => {
     };
   }, [user, navigate, isError, message, dispatch]);
 
+  const renderAffirmations = () => {
+    return affirmations.map((affirm, idx) => {
+      const { affirmation, startMood, endMood, createdAt, updatedAt } = affirm;
+      console.log(affirmation, startMood, endMood, createdAt, updatedAt);
+      const moodIcon = moodIcons[startMood];
+      return (
+        <div className='affirmation-card' key={idx}>
+          <div className='image'>
+            <img
+              src={
+                endMood
+                  ? affirmationImages[endMood]
+                  : affirmationImages[startMood]
+              }
+              alt='mood-image'
+            />
+          </div>
+          <div className='affirmation'>
+            <h4 className='affirmation-text'>{affirmation}</h4>
+            <div className='affirmation-moods'>
+              <i
+                className={`${moodIcon} start-mood-icon ${startMood}`}
+                title='Affirmations starting mood'
+              ></i>
+              {endMood ? (
+                <>
+                  {/* Title attribute will give a little visual feedback tooltip on what the icon is */}
+                  <i
+                    className={`${moodIcon} end-mood-icon ${startMood}`}
+                    title='Affirmations ending mood'
+                  ></i>
+                </>
+              ) : null}
+            </div>
+          </div>
+          <div className='update-box'>
+            <i
+              className='fa-solid fa-pen-to-square'
+              title='Delete this Affirmation'
+            ></i>
+            <i
+              className='fa-solid fa-trash'
+              title='Delete this Affirmation'
+            ></i>
+          </div>
+        </div>
+      );
+    });
+  };
+
   // console.log(affirmations);
   return (
     <div className='container'>
@@ -54,7 +107,9 @@ const Dashboard = () => {
           <p>Go ahead and create one above!</p>
         </div>
       ) : (
-        <section className='dashboard-affirmations-container'></section>
+        <section className='dashboard-affirmations-container'>
+          {renderAffirmations()}
+        </section>
       )}
     </div>
   );
