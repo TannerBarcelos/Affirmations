56cbf1b64e5a6259c5278822a17d0b4acfa83071
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2022-10-05 13:10:45 -0700

    Changing up some formatting in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,16 @@ const cors = require('cors')
 connectDB()
 
 const app = express()
+
 const PORT = process.env.NODE_DOCKER_PORT || 4000
-const v = process.env.VERSION
+const VERSION = process.env.VERSION
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(`/api/${v}/affirmations`, require('./routes/affirmationRoutes'))
-app.use(`/api/${v}/users`, require('./routes/userRoutes'))
+app.use(`/api/${VERSION}/affirmations`, require('./routes/affirmationRoutes'))
+app.use(`/api/${VERSION}/users`, require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
