2a98228b511a8b48bacbab5ed09965257e84fff7
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-04-05 18:28:12 -0700

    Updating all client side code to get back to working state

---
+++ frontend/src/assets/icons/moodIcons.js
@@ -0,0 +1,10 @@
+const moodIcons = {
+    happy: 'fa-solid fa-face-smile',
+    sad: 'fa-solid fa-face-frown',
+    mad: 'fa-solid fa-face-angry',
+    heartbreak: 'fa-solid fa-heart-crack',
+    hysterical: 'fa-solid fa-face-grin-squint-tears',
+    blah: 'fa-solid fa-face-meh',
+}
+
+export default moodIcons
---
+++ frontend/src/assets/images/affirmationImages.js
@@ -0,0 +1,13 @@
+const affirmationImages = {
+    sad: 'https://images.unsplash.com/photo-1476782916354-326ab24c93df?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734',
+    happy:
+        'https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928',
+    mad: 'https://images.unsplash.com/photo-1453738773917-9c3eff1db985?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+    heartbreak:
+        'https://images.unsplash.com/reserve/dPfjQTyJSJ2LpM7D9Yr0_Photo%2015-02-2014.jpg?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+    hysterical:
+        'https://images.unsplash.com/photo-1515536765-9b2a70c4b333?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872',
+    blah: 'https://images.unsplash.com/photo-1531262951893-05a0ffb31b27?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+}
+
+export default affirmationImages
