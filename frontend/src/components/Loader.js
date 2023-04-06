fb694c8d31f40e0cde5c48fc859b04085be25b69
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-04-05 21:28:35 -0700

    Updates

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import GridLoader from 'react-spinners/GridLoader'
 
-const Loader = ({ loading }) => {
-  let [color] = useState('#417d7a')
+const Loader = ( { loading } ) => {
+  let [color] = useState( '#417d7a' )
   return <GridLoader color={color} loading={loading} size={150} />
 }
 export default Loader
